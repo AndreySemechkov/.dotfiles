11d13b1bba5db9232b4389f0491bcb556a6a0f89
Andrey_Semetchkov <andrey.semetchkov@forter.com>
2022-10-05 21:05:54 +0300

    ssh config and refresh

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# install my brew software
+# install my brew software, refresh Brewfile periodically using > brew bundle dump
 brew bundle --file ~/.dotfiles/Brewfile
 # install z shell
 echo "$(which zsh)" | sudo tee -a /etc/shells
 echo "Changing do zsh"
 chsh -s $(which zsh)
+#config ssh
+mkdir ~/.ssh
+cp ~/.dotfiles/.ssh-example ~/.ssh/myconfig
 # symlinking .dotfiles repo to sync with home
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.zshrc  ~/.zshrc
