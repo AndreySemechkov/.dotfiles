38d604e884c30b41ea8bbef18c269d9d5e708ad4
Andrey Semetchkov <andrey.semetchkov.office@gmail.com>
2024-11-14 23:06:03 +0200

    add ohmyzsh to init.sh

--- init.sh
+++ init.sh
@@ -2,13 +2,14 @@
 xcode-select --install
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-git clone git@github.com:AndreySemechkov/.dotfiles.git ~/.dotfiles
 # install my brew software, refresh Brewfile periodically using > brew bundle dump
 brew bundle --file ~/.dotfiles/mac/Brewfile
 # install z shell
 echo "$(which zsh)" | sudo tee -a /etc/shells
-echo "Changing do zsh"
+echo "Changing default to zsh"
 chsh -s $(which zsh)
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # turn on fzf history autocomplete
 /opt/homebrew/opt/fzf/install
 #config ssh
